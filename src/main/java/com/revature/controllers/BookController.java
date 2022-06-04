1cf8908ae1213f637755c716c7c8e0ff5f8da116
yuemcn <yue.mcn@gmail.com>
2022-06-04 15:23:12 -0400

    added Log4j and changed email, application.properties

--- src/main/java/com/revature/controllers/BookController.java
+++ src/main/java/com/revature/controllers/BookController.java
@@ -2,6 +2,7 @@ package com.revature.controllers;
 
 import com.revature.models.Book;
 import com.revature.services.BookService;
+import com.revature.utils.LoggingUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,9 +28,11 @@ public class BookController {
             long isbn = Long.parseLong(body.get("isbn"));
             int yearPublished = Integer.parseInt(body.get("yearPublished"));
             Book book = bs.createBook(body.get("title"), body.get("author"), genreId, body.get("summary"), isbn, yearPublished);
+            LoggingUtil.logger.info("Successfully created a new book: " + body.get("title"));
             return new ResponseEntity<>(book, HttpStatus.CREATED);
         } catch (Exception e) {
             e.printStackTrace();
+            LoggingUtil.logger.info("Failed attempt to create a new book");
             return new ResponseEntity<>("Book already exists", HttpStatus.CONFLICT);
         }
     }
@@ -37,9 +40,11 @@ public class BookController {
     @GetMapping("/book/get-all-books")
     public ResponseEntity<Object> handleGetAllBooks() {
         try {
+            LoggingUtil.logger.info("Successfully retrieved all books");
             return new ResponseEntity<>(bs.getAllBooks(), HttpStatus.ACCEPTED);
         } catch (Exception e) {
             e.printStackTrace();
+            LoggingUtil.logger.info("Could not retrieve all books");
             return new ResponseEntity<>("Could not get all books", HttpStatus.CONFLICT);
         }
     }
@@ -48,9 +53,11 @@ public class BookController {
     public ResponseEntity<Object> handleGetBooksByTitle(@PathVariable("title") String title) {
         try {
             title.replaceAll("%20", " ");
+            LoggingUtil.logger.info("Successfully retrieved book(s) with title " + title);
             return new ResponseEntity<>(bs.getBookByTitle(title), HttpStatus.ACCEPTED);
         } catch (Exception e) {
             e.printStackTrace();
+            LoggingUtil.logger.info("Could not retrieve book(s) with title " + title);
             return new ResponseEntity<>("Could not find this book", HttpStatus.NOT_FOUND);
         }
     }
@@ -59,27 +66,33 @@ public class BookController {
     public ResponseEntity<Object> handleGetBooksByAuthor(@PathVariable("author") String author) {
         try {
             author.replaceAll("%20", " ");
+            LoggingUtil.logger.info("Successfully retrieved all books from author " + author);
             return new ResponseEntity<>(bs.getBookByAuthor(author), HttpStatus.ACCEPTED);
         } catch (Exception e) {
             e.printStackTrace();
+            LoggingUtil.logger.info("Could not retrieve all books from author " + author);
             return new ResponseEntity<>("Could not find this book", HttpStatus.NOT_FOUND);
         }
     }
     @GetMapping("/book/get-books-by-isbn/{isbn}")
     public ResponseEntity<Object> handleGetBooksByIsbn(@PathVariable("isbn") long isbn) {
         try {
+            LoggingUtil.logger.info("Successfully retrieved book with ISBN " + isbn);
             return new ResponseEntity<>(bs.getBookByIsbn(isbn), HttpStatus.ACCEPTED);
         } catch (Exception e) {
             e.printStackTrace();
+            LoggingUtil.logger.info("Could not retrieve book with ISBN " + isbn);
             return new ResponseEntity<>("Could not find this book", HttpStatus.NOT_FOUND);
         }
     }
     @GetMapping("/book/get-books-by-genreId/{genreId}")
     public ResponseEntity<Object> handleGetBooksByGenreId(@PathVariable("genreId") int genreId) {
         try {
+            LoggingUtil.logger.info("Successfully retrieved all books with genre " + genreId);
             return new ResponseEntity<>(bs.getBookByGenreId(genreId), HttpStatus.ACCEPTED);
         } catch (Exception e) {
             e.printStackTrace();
+            LoggingUtil.logger.info("Could not retrieve all books with genre " + genreId);
             return new ResponseEntity<>("Could not find this book", HttpStatus.NOT_FOUND);
         }
     }
@@ -90,9 +103,11 @@ public class BookController {
             long isbn = Long.parseLong(body.get("isbn"));
             bs.deleteBookByIsbn(isbn);
             String message = "Book with isbn " + isbn + " has been delete";
+            LoggingUtil.logger.info("Successfully deleted book with ISBN " + isbn);
             return new ResponseEntity<>(message, HttpStatus.ACCEPTED);
         } catch (Exception e) {
             e.printStackTrace();
+            LoggingUtil.logger.info("Could not delete book with ISBN " + Long.parseLong(body.get("isbn")));
             return new ResponseEntity<>("Could not find this book", HttpStatus.NOT_FOUND);
         }
     }
@@ -100,9 +115,11 @@ public class BookController {
     @GetMapping("/book/get-books-most-popular")
     public ResponseEntity<Object> handleGetBookPopular() {
         try {
+            LoggingUtil.logger.info("Successfully retrieved most popular books");
             return new ResponseEntity<>(bs.getMostPopularBooks(), HttpStatus.ACCEPTED);
         } catch (Exception e) {
             e.printStackTrace();
+            LoggingUtil.logger.info("Could not retrieve most popular books");
             return new ResponseEntity<>("There is no book in library", HttpStatus.NOT_FOUND);
         }
     }
@@ -115,9 +132,11 @@ public class BookController {
             long isbn = Long.parseLong(body.get("isbn"));
             int yearPublished = Integer.parseInt(body.get("yearPublished"));
             Book book = bs.updateBook(id, body.get("title"), body.get("author"), genreId, body.get("summary"), isbn, yearPublished);
+            LoggingUtil.logger.info("Successfully updated book with ID# " + id);
             return new ResponseEntity<>(book, HttpStatus.ACCEPTED);
         } catch (Exception e) {
             e.printStackTrace();
+            LoggingUtil.logger.info("Could not update book with ID# " + Integer.parseInt(body.get("bookId")));
             return new ResponseEntity<>("Could not update book", HttpStatus.NOT_FOUND);
         }
     }
@@ -125,9 +144,11 @@ public class BookController {
     @GetMapping("/book/recent")
     public ResponseEntity<Object> handleGetRecentBooks() {
         try {
+            LoggingUtil.logger.info("Successfully retrieved recently added books");
             return new ResponseEntity<>(bs.getRecentBooks(), HttpStatus.ACCEPTED);
         } catch (Exception e) {
             e.printStackTrace();
+            LoggingUtil.logger.info("Could not retrieve recently added books");
             return new ResponseEntity<>("Could not find most recent books", HttpStatus.NOT_ACCEPTABLE);
         }
     }
@@ -135,9 +156,11 @@ public class BookController {
     @GetMapping("/book/search/{input}")
         public ResponseEntity<Object> handleGetBooksByGenreId(@PathVariable("input") String input) {
             try {
+                LoggingUtil.logger.info("Successfully searched books with input " + input);
                 return new ResponseEntity<>(bs.searchBooks(input), HttpStatus.ACCEPTED);
             } catch (Exception e) {
                 e.printStackTrace();
+                LoggingUtil.logger.info("Could not search for books with input " + input);
                 return new ResponseEntity<>("Book Search Failed", HttpStatus.NOT_FOUND);
             }
         }
--- src/main/java/com/revature/controllers/UserController.java
+++ src/main/java/com/revature/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.revature.controllers;
 
 import com.revature.models.User;
 import com.revature.services.UserService;
+import com.revature.utils.LoggingUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,9 +26,11 @@ public class UserController {
     public ResponseEntity<Object> handleRegisterUser(@RequestBody LinkedHashMap<String, String> body){
         try{
             User u = us.registerNewUser(body.get("email"), body.get("password"), body.get("firstName"), body.get("lastName"));
+            LoggingUtil.logger.info("Successfully registered with email " + body.get("email"));
             return new ResponseEntity<>(u, HttpStatus.CREATED);
         }catch(Exception e){
             e.printStackTrace();
+            LoggingUtil.logger.info("Could not register user");
             return new ResponseEntity<>("Email already taken.", HttpStatus.CONFLICT);
         }
     }
@@ -37,8 +40,10 @@ public class UserController {
         String email = body.get("email");
         String password = body.get("password");
         try{
+            LoggingUtil.logger.info("Successfully logged in user " + email);
             return new ResponseEntity<>(us.loginUser(email, password), HttpStatus.ACCEPTED);
         }catch(Exception e){
+            LoggingUtil.logger.info("Could not log in user " + email);
             return new ResponseEntity<>("Invalid email or password", HttpStatus.NOT_ACCEPTABLE);
         }
     }
@@ -51,9 +56,11 @@ public class UserController {
         String firstName = body.get("firstName");
         String lastName = body.get("lastName");
         try{
+            LoggingUtil.logger.info("Successfully edited user " + email);
             return new ResponseEntity<>(us.updateUser(id, email, password, firstName, lastName), HttpStatus.ACCEPTED);
         }catch(Exception e){
             e.printStackTrace();
+            LoggingUtil.logger.info("Could not edit user " + email);
             return new ResponseEntity<>("Invalid email or password", HttpStatus.NOT_ACCEPTABLE);
         }
     }
@@ -62,8 +69,10 @@ public class UserController {
     public ResponseEntity<Object> handleFindUserById(@RequestBody LinkedHashMap<String, String> body) {
         int id = Integer.parseInt(body.get("userId"));
         try {
+            LoggingUtil.logger.info("Successfully viewed user with ID# " + id);
             return new ResponseEntity<>(us.findUserById(id), HttpStatus.ACCEPTED);
         } catch (Exception e) {
+            LoggingUtil.logger.info("Could not view user with ID# " + id);
             return new ResponseEntity<>("Invalid user id", HttpStatus.NOT_ACCEPTABLE);
         }
     }
@@ -71,8 +80,10 @@ public class UserController {
     @GetMapping("/user/all-users")
     public ResponseEntity<Object> handleGetAllUsers() {
         try {
+            LoggingUtil.logger.info("Successfully viewed all users");
             return new ResponseEntity<>(us.getAllUsers(), HttpStatus.ACCEPTED);
         } catch (Exception e) {
+            LoggingUtil.logger.info("Could not view all users");
             return new ResponseEntity<>("Could not get all users", HttpStatus.NOT_ACCEPTABLE);
         }
     }
@@ -80,9 +91,11 @@ public class UserController {
     @GetMapping("/user/checkout-show/{userId}")
     public ResponseEntity<Object> handleCheckOutShow(@PathVariable("userId") int userId){
         try {
+            LoggingUtil.logger.info("Successfully showed checked out books from user with ID# " + userId);
             return new ResponseEntity<>(us.getCheckedOutBooks(userId), HttpStatus.ACCEPTED);
         } catch (Exception e) {
             e.printStackTrace();
+            LoggingUtil.logger.info("Could not show checked out books from user with ID# " + userId);
             return new ResponseEntity<>("Could not find user", HttpStatus.CONFLICT);
         }
     }
@@ -92,13 +105,14 @@ public class UserController {
         try {
             int userId = Integer.parseInt(body.get("userId"));
             long isbn = Long.parseLong(body.get("isbn"));
-
             us.checkOutBook(userId, isbn);
-            String message = "Book has been checked out" ;
+            String message = "Book has been checked out";
+            LoggingUtil.logger.info("User with ID# " + userId + " successfully checked out book with ISBN " + isbn);
             return new ResponseEntity<>(us.getCheckedOutBooks(userId), HttpStatus.ACCEPTED);
 
         } catch (Exception e) {
             e.printStackTrace();
+            LoggingUtil.logger.info("User with ID# " + Integer.parseInt(body.get("userId")) + " could not check out book with ISBN " + Long.parseLong(body.get("isbn")));
             return new ResponseEntity<>("Could not check out book", HttpStatus.CONFLICT);
         }
     }
